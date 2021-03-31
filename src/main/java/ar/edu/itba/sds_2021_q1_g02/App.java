d0618197f00f8dbe9096ecc1573f3126e9ceafbf
nicbritos <nicbritos@hotmail.com>
2021-03-31 09:49:06 -0300

    Added 3D simulation

--- src/main/java/ar/edu/itba/sds_2021_q1_g02/App.java
+++ src/main/java/ar/edu/itba/sds_2021_q1_g02/App.java
@@ -1,82 +1,27 @@
 package ar.edu.itba.sds_2021_q1_g02;
 
 import ar.edu.itba.sds_2021_q1_g02.models.CellularParticle;
-import ar.edu.itba.sds_2021_q1_g02.models.Position;
+import ar.edu.itba.sds_2021_q1_g02.parsers.CommandParser;
+import ar.edu.itba.sds_2021_q1_g02.parsers.ParticleParser;
+import javafx.util.Pair;
+import org.apache.commons.cli.ParseException;
 
+import java.io.IOException;
 import java.util.*;
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-
 
 public class App {
-    public static void main(String[] args) {
-        List<CellularParticle> particles = new ArrayList<CellularParticle>();
-        System.out.println(args[0]);
-        int i = 0;
-        try {
-            File myObj = new File(args[0]);
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                String[] info = data.split("\t");
-                CellularParticle particle = new CellularParticle(i, Double.parseDouble(info[0]), Integer.parseInt(info[1]), Integer.parseInt(info[4]) == 1 ? true : false);
-                particles.add(particle);
-                particles.get(i).setPosition(new Position(Integer.parseInt(info[2]), Integer.parseInt(info[3])));
-                i++;
+    public static void main(String[] args) throws ParseException, IOException {
+        CommandParser.getInstance().parse(args);
 
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+        Pair<List<CellularParticle>, Integer> particles = ParticleParser.parseParticles(CommandParser.getInstance().getInputPath(), CommandParser.getInstance().isEnable3D());
 
-//        List<CellularParticle> particles = Arrays.asList(
-//                new CellularParticle(1, 1, 1, false),
-//                new CellularParticle(2, 1, 1, false),
-//                new CellularParticle(3, 1, 1, false),
-//                new CellularParticle(4, 1, 1, false),
-//                new CellularParticle(5, 1, 1, false),
-//                new CellularParticle(6, 1, 1, false),
-//                new CellularParticle(7, 1, 1, true),
-//                new CellularParticle(8, 1, 1, false),
-//                new CellularParticle(9, 1, 1, true),
-//                new CellularParticle(10, 1, 1, false),
-//                new CellularParticle(11, 1, 1, false),
-//                new CellularParticle(12, 1, 1, true),
-//                new CellularParticle(13, 1, 1, true),
-//                new CellularParticle(14, 1, 1, true),
-//                new CellularParticle(15, 1, 1, false),
-//                new CellularParticle(16, 1, 1, true),
-//                new CellularParticle(17, 1, 1, true),
-//                new CellularParticle(18, 1, 1, false),
-//                new CellularParticle(19, 1, 1, false),
-//                new CellularParticle(20, 1, 1, true),
-//                new CellularParticle(21, 1, 1, false),
-//                new CellularParticle(22, 1, 1, false),
-//                new CellularParticle(23, 1, 1, false),
-//                new CellularParticle(24, 1, 1, false),
-//                new CellularParticle(25, 1, 1, false)
-//        );
-//        int i = 0;
-//        for (int x = 0; x < 5; x++) {
-//            for (int y = 0; y < 5; y++) {
-//                particles.get(i).setPosition(new Position(x, y));
-//                i++;
-//            }
-//        }
-//        int idx = 1;
-//        for (CellularParticle particle : particles) {
-//            System.out.print(" [" + particle.getState() + "] ");
-//            if (idx % 110 == 0) {
-//                System.out.println();
-//            }
-//            idx++;
-//        }
-        GameOfLife GOL = new GameOfLife(particles, 110);
-        GOL.simulate2D(10);
+        GameOfLife GOL = new GameOfLife(particles.getKey(), particles.getValue());
+
+        if (CommandParser.getInstance().isEnable3D())
+            GOL.simulate3D(CommandParser.getInstance().getMaxIterations());
+        else
+            GOL.simulate2D(CommandParser.getInstance().getMaxIterations());
     }
-
 }
 
 
--- src/main/java/ar/edu/itba/sds_2021_q1_g02/GameOfLife.java
+++ src/main/java/ar/edu/itba/sds_2021_q1_g02/GameOfLife.java
@@ -23,7 +23,7 @@ public class GameOfLife {
 
     //MaxIteration como metodo de corte
     public void simulate2D(int maxIterations) {
-        Grid grid = new Grid2D(this.M);
+        Grid2D grid = new Grid2D(this.M);
         for (int i = 0; i < maxIterations; i++) {
             Map<Particle, State> nextStates = new HashMap<>();
             grid.populateGrid(this.particles);
@@ -31,7 +31,7 @@ public class GameOfLife {
             for (int x = 0; x < M; x++) {
                 for (int y = 0; y < M; y++) {
                     Position currentPosition = new Position(x, y);
-                    int neighborsAlive = getTotalNeighborsAlive((Particle[][]) grid.getGrid(), currentPosition);
+                    int neighborsAlive = getTotalNeighborsAlive(grid.getGrid(), currentPosition);
                     Pair<State, Boolean> stateUpdated =
                             Rules.applyRules(((CellularParticle) grid.getParticle(currentPosition)).getState(),
                                     neighborsAlive);
@@ -47,7 +47,7 @@ public class GameOfLife {
             }
 
             try {
-                createOutputFile(this.particles, i);
+                createOutputFile(this.particles, i, false);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -64,23 +64,59 @@ public class GameOfLife {
         }
     }
 
-    private void deletePastFiles(){
-        File file = new File("./ovito.1.txt");
-        int i = 2;
-        while (file.delete()){
-            file = new File("./ovito." + i+ ".txt");
-            i+=1;
+    //MaxIteration como metodo de corte
+    public void simulate3D(int maxIterations) {
+        Grid3D grid = new Grid3D(this.M);
+        for (int i = 0; i < maxIterations; i++) {
+            Map<Particle, State> nextStates = new HashMap<>();
+            grid.populateGrid(this.particles);
+
+            for (int x = 0; x < M; x++) {
+                for (int y = 0; y < M; y++) {
+                    for (int z = 0; z < M; z++) {
+                        Position currentPosition = new Position(x, y, z);
+                        int neighborsAlive = this.getTotalNeighborsAlive(grid.getGrid(), currentPosition);
+                        Pair<State, Boolean> stateUpdated =
+                                Rules.applyRules(((CellularParticle) grid.getParticle(currentPosition)).getState(),
+                                        neighborsAlive);
+                        if (stateUpdated.getValue()) {
+                            nextStates.put(grid.getParticle(currentPosition), stateUpdated.getKey());
+                        }
+                    }
+                }
+            }
+            for (CellularParticle particle : this.particles) {
+                if (nextStates.containsKey(particle)) {
+                    updateParticleState(particle, nextStates.get(particle));
+                }
+            }
+
+            try {
+                createOutputFile(this.particles, i, true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //TODO: Delete when already tested
+            int idx = 1;
+//            System.out.println("------- ITERATION:" + i + "-----------");
+//            for (CellularParticle particle : this.particles) {
+//                System.out.print(" [" + particle.getState() + "] ");
+//                if (idx % 5 == 0) {
+//                    System.out.println();
+//                }
+//                idx++;
+//            }
         }
     }
 
-    private void createOutputFile(List<CellularParticle> particles, int i) throws IOException {
-
+    private void createOutputFile(List<CellularParticle> particles, int i, boolean includeZ) throws IOException {
         List<CellularParticle> livingParticles = particles.stream()
                 .filter(particle -> State.ALIVE.equals(particle.getState()))
                 .collect(Collectors.toList());
 
         // create new file
-        File file = new File("./ovito."+ i + ".txt");
+        String filePath = "./ovito." + (includeZ ? "3d." : "2d.") + i + ".txt";
+        File file = new File(filePath);
         if (!file.createNewFile()) {
             file.delete();
             file.createNewFile();
@@ -88,27 +124,28 @@ public class GameOfLife {
 
 
         // write on file
-        FileWriter myFile = new FileWriter("./ovito."+ i +".txt");
+        FileWriter myFile = new FileWriter(filePath);
         myFile.write(Long.toString(livingParticles.stream().count()));
         myFile.write('\n');
         myFile.write('\n');
 
-        System.out.println(Long.toString((livingParticles.stream().count())));
         long count = livingParticles.stream().count();
         for(int j=0; j<count ; j++){
-
             myFile.write(Double.toString(livingParticles.get(j).getRadius()));
             myFile.write('\t');
             myFile.write(Double.toString(livingParticles.get(j).getPosition().getX()));
             myFile.write('\t');
             myFile.write(Double.toString(livingParticles.get(j).getPosition().getY()));
             myFile.write('\n');
+
+            if (includeZ) {
+                myFile.write(Double.toString(livingParticles.get(j).getPosition().getZ()));
+                myFile.write('\n');
+            }
         }
 
 
         myFile.close();
-
-
     }
 
     private void updateParticleState(CellularParticle particle, State nextState) {
@@ -124,6 +161,30 @@ public class GameOfLife {
             int neighbor_y = (int) (position.getY() + neighborPosition.getY());
             if ((neighbor_x >= 0) && (neighbor_x < grid.length) && (neighbor_y >= 0) && (neighbor_y < grid.length)) {
                 State state = ((CellularParticle) grid[neighbor_x][neighbor_y]).getState();
+                if (state.equals(State.ALIVE)) {
+                    neighborsAlive++;
+                }
+            }
+        }
+        return neighborsAlive;
+    }
+
+    private int getTotalNeighborsAlive(Particle[][][] grid, Position position) {
+        MooreNeighborhood mooreNeighborhood = new MooreNeighborhood(MOORE_NEIGHBORHOOD_RADIUS);
+        List<Position> neighborsPositions = mooreNeighborhood.getPositions3D();
+
+        int neighborsAlive = 0;
+        for (Position neighborPosition : neighborsPositions) {
+            int neighbor_x = (int) (position.getX() + neighborPosition.getX());
+            int neighbor_y = (int) (position.getY() + neighborPosition.getY());
+            int neighbor_z = (int) (position.getZ() + neighborPosition.getZ());
+
+            if (
+                    (neighbor_x >= 0) && (neighbor_x < grid.length)
+                    && (neighbor_y >= 0) && (neighbor_y < grid.length)
+                    && (neighbor_z >= 0) && (neighbor_z < grid.length)
+            ) {
+                State state = ((CellularParticle) grid[neighbor_x][neighbor_y][neighbor_z]).getState();
                 if (state.equals(State.ALIVE)) {
                     neighborsAlive++;
                 }
